6823d69d21faaa6807f59e40e9efc89d7ace776e
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-09-07 01:07:16 +0900

    singleton Pattern & create service file for business logic

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,53 @@
 import * as express from "express";
 import catsRouter from "./cats/cats.route";
 
-const app: express.Express = express();
+class Server {
+    public app: express.Application;
 
-// * logging middleware
-// 미들웨어는 use메서드를 사용해서 만든다. 세번째 인자 next는 이 함수가 다음 라우터로 이동할수 있는 함수이다.
-app.use((req, res, next) => {
-    console.log(req.rawHeaders[0]);
-    console.log("this is logging middleware");
-    next(); // 프런트에서 들어와서 미들웨어를 한번 걸치고 next()를 통해 라우터로 이동한다.
-});
-// 만약에 미들웨어가 가장 마지막에 작동한다면 아무런 로깅이 찍히지 않는다. 익스프레스가 위에서 내려오면서 원하는 엔드포인트를 찾으면 통신을 끊어버리기 때문이다. 이렇게 때문에 미들웨어는 순서가 중요하다.
+    constructor() {
+        const app: express.Application = express();
+        this.app = app;
+    }
 
-// * json middleware
-//express에서 body(json)를 읽을수 있도록 미들웨어를 추가해줘야한다.
-app.use(express.json());
+    private setRoute() {
+        this.app.use(catsRouter);
+    }
 
-app.use(catsRouter);
+    private setMiddleware() {
+        // * logging middleware
+        // 미들웨어는 use메서드를 사용해서 만든다. 세번째 인자 next는 이 함수가 다음 라우터로 이동할수 있는 함수이다.
+        this.app.use((req, res, next) => {
+            console.log(req.rawHeaders[0]);
+            console.log("this is logging middleware");
+            next(); // 프런트에서 들어와서 미들웨어를 한번 걸치고 next()를 통해 라우터로 이동한다.
+        });
+        // 만약에 미들웨어가 가장 마지막에 작동한다면 아무런 로깅이 찍히지 않는다. 익스프레스가 위에서 내려오면서 원하는 엔드포인트를 찾으면 통신을 끊어버리기 때문이다. 이렇게 때문에 미들웨어는 순서가 중요하다.
 
-// * 404 middleware
-// 아무 라우터에 해당이 되지 않는다면 마지막에 에러를 잡는 미들웨어를 생성한다.
-app.use((req, res, next) => {
-    console.log("this is error middleware");
-    res.send({ error: "404 not found error" });
-});
+        // * json middleware
+        //express에서 body(json)를 읽을수 있도록 미들웨어를 추가해줘야한다.
+        this.app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("server is on ... ");
-});
+        this.setRoute();
+
+        // * 404 middleware
+        // 아무 라우터에 해당이 되지 않는다면 마지막에 에러를 잡는 미들웨어를 생성한다.
+        this.app.use((req, res, next) => {
+            console.log("this is error middleware");
+            res.send({ error: "404 not found error" });
+        });
+    }
+
+    public listen() {
+        this.setMiddleware();
+        this.app.listen(3000, () => {
+            console.log("server is on ... ");
+        });
+    }
+}
+
+function init() {
+    const server = new Server();
+    server.listen();
+}
+
+init();
--- src/cats/cats.route.ts
+++ src/cats/cats.route.ts
@@ -1,130 +1,32 @@
 import { Cat, CatType } from "./cats.model";
 import { Router } from "express";
+import {
+    readAllcat,
+    readCat,
+    updatePartialCat,
+    createCat,
+    updateCat,
+    deleteCat,
+} from "./cats.service";
 
 const router = Router();
 
 // * READ 고양이 전체 데이터 다 조회
-router.get("/cats", (req, res) => {
-    try {
-        const cats = Cat;
-        // throw new Error("db connect error");
-        res.status(200).send({
-            success: true,
-            data: {
-                cats,
-            },
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+router.get("/cats", readAllcat);
 
-// * READ 특정 고양이 데이터 조회
-// :id는 파라미터가 되어서 동적라우팅이 된다.
-router.get("/cats/:id", (req, res) => {
-    try {
-        const params = req.params;
-        console.log(params);
-        const cat = Cat.find((cat) => {
-            return cat.id === params.id;
-        });
-        // throw new Error("db connect error");
-        res.status(200).send({
-            success: true,
-            data: {
-                cat,
-            },
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+// * READ 고양이 특정 데이터 조회
+router.get("cats/:id", readCat);
 
 // * CREATE 새로운 고양이 추가 api
-router.post("/cats", (req, res) => {
-    try {
-        const data = req.body;
-        Cat.push(data); // CREATE
-        res.status(200).send({
-            success: true,
-            data: { data },
-        });
-    } catch (error) {}
-});
+router.post("/cats", createCat);
 
 // * UPDATE 고양이 데이터 업데이트 -> PUT
-router.put("/cats/:id", (req, res) => {
-    try {
-        const params = req.params;
-        const body = req.body;
-        let result;
-        Cat.forEach((cat) => {
-            if (cat.id === params.id) {
-                cat = body;
-                result = cat;
-            }
-        });
-        res.status(200).send({
-            success: true,
-            data: {
-                cat: result,
-            },
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+router.put("/cats/:id", updateCat);
 
 // * UPDATE 고양이 데이터 부분적으로 업데이트 -> PATCH
-router.patch("/cats/:id", (req, res) => {
-    try {
-        const params = req.params;
-        const body = req.body;
-        let result;
-        Cat.forEach((cat) => {
-            if (cat.id === params.id) {
-                cat = { ...cat, ...body }; // 구조분해 할당 = 원래 있었던 데이터를 모두 구조분해를 시키고 새롭게 올라온 데이터도 구조분해를 시키면 기존의 key에서 중복된 key에대한 value값을 바꿔준다.
-                result = cat;
-            }
-        });
-        res.status(200).send({
-            success: true,
-            data: {
-                cat: result,
-            },
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+router.patch("/cats/:id", updatePartialCat);
 
 // * DELETE 고양이 데이터 삭제 -> DELETE
-router.delete("/cats/:id", (req, res) => {
-    try {
-        const params = req.params;
-        const newCat = Cat.filter((cat) => cat.id !== params.id);
-        res.status(200).send({
-            success: true,
-            data: newCat,
-        });
-    } catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+router.delete("/cats/:id", deleteCat);
 
 export default router;
