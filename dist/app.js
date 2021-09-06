1517c2d7f932eb5a8706ecf7c87ccf75f03323bd
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-09-06 15:11:40 +0900

    comprehension of Middlewares

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
-var port = 3000;
-app.get("/", function (req, res) {
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[0]);
+    console.log("this is logging middleware");
+    next();
+});
+app.get("/cats/blue", function (req, res, next) {
+    console.log(req.rawHeaders[0]);
+    console.log("this is blue middleware");
+    next();
+});
+app.get("/cats", function (req, res) {
     console.log(req);
-    res.send("Hello World!");
+    res.send({ cats: app_model_1.Cat });
 });
-app.post("/test", function (req, res) {
-    res.send({ person: "seol" });
+app.get("/cats/blue", function (req, res) {
+    res.send({ blue: app_model_1.Cat[0] });
 });
-app.listen(port, function () {
-    console.log("Example app listening at http://localhost:" + port);
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
+});
+app.listen(3000, function () {
+    console.log("server is on ... ");
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,38 @@
 import * as express from "express";
+import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
-// 여기서 app은 express의 인스턴스라고 생각하면된다. app이 곧 서버역할을 한다.
-const port: number = 3000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
+// 미들웨어는 use메서드를 사용해서 만든다. 세번째 인자 next는 이 함수가 다음 라우터로 이동할수 있는 함수이다.
+app.use((req, res, next) => {
+    console.log(req.rawHeaders[0]);
+    console.log("this is logging middleware");
+    next(); // 프런트에서 들어와서 미들웨어를 한번 걸치고 next()를 통해 라우터로 이동한다.
+});
+// 만약에 미들웨어가 가장 마지막에 작동한다면 아무런 로깅이 찍히지 않는다. 익스프레스가 위에서 내려오면서 원하는 엔드포인트를 찾으면 통신을 끊어버리기 때문이다. 이렇게 때문에 미들웨어는 순서가 중요하다.
+
+// 이렇게 콜백함수에 next을 넣게 되면 아래쪽 같은 라우터에 넘어가게 된다. (미들웨어처럼 동작을 한다)
+app.get("/cats/blue", (req, res, next) => {
+    console.log(req.rawHeaders[0]);
+    console.log("this is blue middleware");
+    next();
+});
+
+// app.get은 라우터이다. 라우터에 대한 콜백함수에 res,req의 정보가 담기게 되고 res.send메소드를 통해 클라이언트에 리소스를 보내준다.
+app.get("/cats", (req: express.Request, res: express.Response) => {
     console.log(req);
-    res.send("Hello World!"); // send라는 메서드를 통해 response를 보내준다.
+    res.send({ cats: Cat }); // key와 value가 동일하면 하나로 생략할수 있다.
 });
 
-app.post("/test", (req, res) => {
-    res.send({ person: "seol" });
+app.get("/cats/blue", (req, res) => {
+    res.send({ blue: Cat[0] });
 });
 
-app.listen(port, () => {
-    // listen은 서버를 연다라고 생각하면 된다.
-    console.log(`Example app listening at http://localhost:${port}`);
+// 아무 라우터에 해당이 되지 않는다면 마지막에 에러를 잡는 미들웨어를 생성한다.
+app.use((req, res, next) => {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
+});
+
+app.listen(3000, () => {
+    console.log("server is on ... ");
 });
